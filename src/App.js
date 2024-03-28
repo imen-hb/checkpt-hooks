527bbce87f2435b775775e869906fa2c90c5f6b0
imen-hb <imenhbibi502@gmail.com>
2024-03-28 15:33:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
 import { useState } from 'react';
 import './App.css';
 import Navigation from './components/Navigation';
-import MoviesList from './components/MovieList.js';
-import {moviesData} from './Data'
-import AddMovie from './components/AddMovie.js';
-import Filter from './components/Filter.js';
-
-
-
+import { moviesData } from './Data';
+import { Route, Routes } from 'react-router-dom';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Movies from './pages/Movies.js';
+import ErrorPage from './pages/ErrorPage';
 
 function App() {
-  const [ movies, setMovies] = useState(moviesData)
+  const [movies, setMovies] = useState(moviesData);
+  const [inputSearch, setInputSearch] = useState('');
+  const [newRate, setNewRate] = useState(0);
 
-const[inputSearch,setInputSearch]= useState("")
-const[newRate,setNewRate]=useState(0)
+  const add = (newMovie) => {
+    setMovies([...movies, newMovie]);
+  };
 
-  const add = (newMovie)=> {
-    setMovies([...movies,newMovie])
-  }
 
   return (
-    <div className="App" style={{backgroundColor: "black"}}>
-      <Navigation/> 
-      <Filter inputSearch={inputSearch} setInputSearch={setInputSearch} newRate={newRate} setNewRate={setNewRate}/>
-      <MoviesList movies={movies} inputSearch={inputSearch} newRate={newRate}/>   
-      <AddMovie add={add}/>  
+    <div className="App" style={{ backgroundColor: 'black' }}>
+      <Navigation />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/movies" element={<Movies 
+          inputSearch={inputSearch} setInputSearch={setInputSearch} newRate={newRate} setNewRate={setNewRate}
+          movies={movies} 
+          add={add} />} />
+        <Route path="/*" element={<ErrorPage />} />
+      </Routes>
+
     </div>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,8 +10,8 @@ return (
           <Navbar.Brand href="#home">Movies-Time</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#movies">Movies</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
